843362a6021cb360e0b3a4f24010b8c31d50426d
dede <andreluis772233@gmail.com>
2025-03-25 21:55:31 -0300

    segundo commit

---
+++ aula3/tabuada.py
@@ -0,0 +1,16 @@
+from flask import flask, render_template, request
+app = Flask(__name__)
+
+@app.route('/')
+def raiz():
+    return render_template('index.html')
+
+@app.route('calcula_tabuada', methods=['POST'])
+def calcula():
+    numero = int(request.form['txt_form'])
+    result ={"tabuada_do" : numero, "valores" : []}
+
+    for i in range(0,11):
+        r = numero * i
+        result['valores'].append(r)
+    return render_template('index.html', resultado = result)
